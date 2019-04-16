3d3723582de18029aa1661e9df54df9f36bfc565
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2019-04-16 19:42:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -315,7 +315,7 @@ function play(guild, song) {
 }
 })
 
-const developers = ["496585065673916417","id"]
+const developers = ["444126346676011028","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
